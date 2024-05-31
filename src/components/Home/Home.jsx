c4b62531bafab22506c1d4f1b89e2f38b267b69b
JuanmaTo <juanmatoniolo2@gmail.com>
2024-05-31 18:32:52 -0300

    Subido a page de git

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./home.css";
-import WhatsAppButton from "../btnWhatsapp/Whatsapp";
 import logo from "../../assets/curiyu-cambios.png";
 
-
 function Home() {
+	const phoneNumber = "+5493412275598";
+	const customMessage = "¡Hola! Estoy interesado en saber más sobre curiyú!";
+
+	const handleWhatsAppClick = () => {
+		const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+			customMessage
+		)}`;
+		window.open(url, "_blank");
+	};
+
 	return (
 		<>
 			<main className="home-container">
 				<header className="club-header">
-					<img
-						src={logo}
-						alt="Logo Curiyú"
-						className="club-logo"
-					/>
+					<img src={logo} alt="Logo Curiyú" className="club-logo" />
 					<h1 className="club-name">Club de Rugby Curiyú</h1>
 				</header>
 				<article className="welcome-section">
@@ -24,8 +28,12 @@ function Home() {
 						con nosotros.
 					</p>
 					<div className="action-buttons">
-						<WhatsAppButton />
-						<button className="join-btn">Únete Ahora</button>
+						<button
+							className="join-btn"
+							onClick={handleWhatsAppClick}
+						>
+							Únete Ahora
+						</button>
 					</div>
 				</article>
 				<section className="club-info-section">
