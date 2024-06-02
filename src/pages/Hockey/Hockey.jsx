5ed475446ac25305598926b90f30aed570ebe2e4
JuanmaTo <juanmatoniolo2@gmail.com>
2024-06-02 02:54:18 -0300

    termine hockey

--- src/pages/Hockey/Hockey.jsx
+++ src/pages/Hockey/Hockey.jsx
@@ -1,16 +1,102 @@
 import React from "react";
+import {/*  Carousel, Image, */ Card } from "react-bootstrap";
 import NavBar from "../../components/nav/Navbar";
+import CallToActionButton from "../../components/btnWhatsapp/Mensaje.jsx";
+import Footer from "../../components/Footer/FooterCuri.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./hockey.css"; // Archivo CSS específico para hockey
+/* import img1 from "../../assets/hockey/1.jpg";
+import img2 from "../../assets/hockey/2.jpg";
+import img3 from "../../assets/hockey/3.jpg";
+import img4 from "../../assets/hockey/4.jpg";
+import img5 from "../../assets/hockey/5.jpg";
+import img6 from "../../assets/hockey/6.jpg";
+import img7 from "../../assets/hockey/7.jpg";
+import img8 from "../../assets/hockey/8.jpg";
+import img9 from "../../assets/hockey/9.jpg";
+import img10 from "../../assets/hockey/10.jpg";
 
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+ */
 function Hockey() {
-    return (
-        <>
-        <NavBar/>
-        <div>
-            <h1>Hockey</h1>
-            <p>Información sobre hockey...</p>
-        </div>
-        </>
-    );
+	return (
+		<>
+			<NavBar />
+			<div className="hockey-container">
+				<h1 className="main-title text-center">
+					CLUB DE HOCKEY CURIYÚ
+				</h1>
+
+				<div className="content-section container">
+					<h2 className="section-title">Descubre el Hockey</h2>
+					<p className="section-description">
+						El hockey es un deporte dinámico que combina velocidad,
+						agilidad y estrategia. Te enseña la importancia del
+						trabajo en equipo y el respeto mutuo, fortaleciendo
+						tanto tu cuerpo como tu mente. Únete a nuestro club de
+						hockey y experimenta la emoción del juego, mejora tu
+						condición física y forma amistades duraderas.
+					</p>
+					<CallToActionButton
+						phoneNumber="3412275598"
+						text="Únete Ahora"
+						className="join-btn"
+					/>
+				</div>
+
+			{/* 	<div className="gallery-section container">
+					<h2 className="section-title">Galería</h2>
+					<Carousel>
+						{images.map((image, index) => (
+							<Carousel.Item key={index}>
+								<Image
+									src={image}
+									className="d-block w-100 gallery-image"
+								/>
+							</Carousel.Item>
+						))}
+					</Carousel>
+				</div> */}
+
+				<div className="schedule-section container">
+					<h2 className="section-title">Horarios de Entrenamiento</h2>
+					<Card className="training-card">
+						<Card.Body>
+							<Card.Title>Hockey Infantil</Card.Title>
+							<Card.Text>
+								Lunes y Miércoles - 17:00 a 18:00 hs
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Card className="training-card">
+						<Card.Body>
+							<Card.Title>Hockey Juvenil</Card.Title>
+							<Card.Text>
+								Martes y Jueves - 18:00 a 19:00 hs
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Card className="training-card">
+						<Card.Body>
+							<Card.Title>Hockey Femenino (Primera)</Card.Title>
+							<Card.Text>
+								Miércoles y Viernes - 19:00 a 20:30 hs
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Card className="training-card">
+						<Card.Body>
+							<Card.Title>Hockey Masculino (Primera)</Card.Title>
+							<Card.Text>
+								Martes y Jueves - 20:00 a 21:30 hs
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default Hockey;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import logo from "../../assets/curiyu-cambios.png";
 import NavBar from "../../components/nav/Navbar";
+import Footer from "../../components/Footer/FooterCuri.jsx";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 function Login() {
-	return (
-		<>
-		<NavBar/>
-		
-		
-        <main className="main-login">
-		<section className="login-section">
-			<header className="club-header">
-				<img src={logo} alt="Logo Curiyú" className="club-logo" />
-				<h1 className="club-name">Club Curiyú</h1>
-			</header>
-			<h2 className="login-title">Iniciar Sesión</h2>
-			<form className="login-form">
-				<div className="input-group">
-					<label htmlFor="username">Usuario:</label>
-					<input type="text" id="username" name="username" />
-				</div>
-				<div className="input-group">
-					<label htmlFor="password">Contraseña:</label>
-					<input type="password" id="password" name="password" />
-				</div>
-				<button type="submit" className="login-btn">
-					Entrar
-				</button>
-			</form>
-		</section>
-        </main>
-		</>
-	);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <>
+            <NavBar />
+
+            <main className="main-login">
+                <section className="login-section">
+                    <header className="club-header">
+                        <img src={logo} alt="Logo Curiyú" className="club-logo" />
+                        <h1 className="club-name">Club Curiyú</h1>
+                    </header>
+                    <h2 className="login-title">Iniciar Sesión</h2>
+                    <form className="login-form">
+                        <div className="input-group">
+                            <label htmlFor="username">Usuario:</label>
+                            <input type="text" id="username" name="username" />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="password">Contraseña:</label>
+                            <div className="password-input-container">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    id="password"
+                                    name="password"
+                                    className="password-input"
+                                />
+                                <div className="password-toggle-btn" onClick={togglePasswordVisibility}>
+                                    {showPassword ? <IoMdEyeOff /> : <IoMdEye />}
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" className="login-btn">
+                            Entrar
+                        </button>
+                    </form>
+                </section>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default Login;
